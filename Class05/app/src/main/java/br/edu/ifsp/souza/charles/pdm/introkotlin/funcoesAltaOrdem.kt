059adbbea4335c4aec10469d8ef83ee0312d0463
souzafcharles <souzach@outlook.com>
2022-09-14 20:13:46 -0300

    Uptade Class 05

--- Class05/app/src/main/java/br/edu/ifsp/souza/charles/pdm/introkotlin/funcoesAltaOrdem.kt
+++ Class05/app/src/main/java/br/edu/ifsp/souza/charles/pdm/introkotlin/funcoesAltaOrdem.kt
@@ -4,10 +4,11 @@ fun processaTexto(
     processa: (String, String) -> String
 ): String = processa(str1, str2)
 
-fun concatena(a: String, b: String): String{
-    return "$a$b"
-}
+fun concatena(a: String, b: String): String = "$a$b"
+
+fun inverte(x: String, y: String) = "${y.reversed()}${x.reversed()}"
 
 fun main() {
     println(processaTexto("Olá, ", "Mundo", ::concatena))
+    println(processaTexto("Olá, ", "Mundo", ::inverte))
 }
