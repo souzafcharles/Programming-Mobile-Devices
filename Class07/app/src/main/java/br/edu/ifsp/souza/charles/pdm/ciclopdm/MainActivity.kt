4807403a6bacb6a5ca4f501a5c50da7d89509e1b
souzafcharles <souzach@outlook.com>
2022-09-21 20:31:01 -0300

    Update Class 07

--- Class07/app/src/main/java/br/edu/ifsp/souza/charles/pdm/ciclopdm/MainActivity.kt
+++ Class07/app/src/main/java/br/edu/ifsp/souza/charles/pdm/ciclopdm/MainActivity.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import br.edu.ifsp.souza.charles.pdm.ciclopdm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    //private val amb: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
 
-    private lateinit var amb : ActivityMainBinding
+    private val amb : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        amb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(amb.root)
     }
 }
