c5f2f77af08b1a10224f722c3c6a6f9910c90cd1
souzafcharles <souzach@outlook.com>
2022-09-28 21:45:25 -0300

    Example Class 08

--- Class08/app/src/main/java/br/edu/ifsp/souza/charles/pdm/intents/MainActivity.kt
+++ Class08/app/src/main/java/br/edu/ifsp/souza/charles/pdm/intents/MainActivity.kt
@@ -3,7 +3,9 @@ package br.edu.ifsp.souza.charles.pdm.intents
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import br.edu.ifsp.souza.charles.pdm.intents.Constant.URL
 import br.edu.ifsp.souza.charles.pdm.intents.databinding.ActivityMainBinding
 
@@ -11,16 +13,28 @@ class MainActivity : AppCompatActivity() {
     private val amb : ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private  lateinit var urlArl: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
         supportActionBar?.subtitle = "MainActivity"
 
+        urlArl = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { resultado: ActivityResult ->
+            if(resultado.resultCode == RESULT_OK){
+                val urlRetornada = resultado.data?.getStringExtra(URL) ?: ""
+                amb.urlTv.text = urlRetornada
+            }
+        }
+
         amb.entrarUrlBt.setOnClickListener {
-            val urlActivityIntent = Intent(this, UrlActivity::class.java)
+//            val urlActivityIntent = Intent(this, UrlActivity::class.java)
+            val urlActivityIntent = Intent("SEGUNDA_TELA_DO_PROJETO_INTENTS")
             urlActivityIntent.putExtra(URL, amb.urlTv.text.toString())
-            startActivity(urlActivityIntent)
+            urlArl.launch(urlActivityIntent)
+//
         }
     }
 }
