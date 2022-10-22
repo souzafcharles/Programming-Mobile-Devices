58acd36bb004306df981f4ab59fdb02081651698
souzafcharles <souzach@outlook.com>
2022-10-21 22:31:53 -0300

    Example Class 09

--- Class09/app/src/main/java/br/edu/ifsp/souza/charles/pdm/mycontacts/view/MainActivity.kt
+++ Class09/app/src/main/java/br/edu/ifsp/souza/charles/pdm/mycontacts/view/MainActivity.kt
@@ -1,12 +1,12 @@
 package br.edu.ifsp.souza.charles.pdm.mycontacts.view
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.souza.charles.pdm.mycontacts.R
 import br.edu.ifsp.souza.charles.pdm.mycontacts.adapter.ContactAdapter
 import br.edu.ifsp.souza.charles.pdm.mycontacts.databinding.ActivityMainBinding
@@ -14,40 +14,38 @@ import br.edu.ifsp.souza.charles.pdm.mycontacts.model.Constant.EXTRA_CONTACT
 import br.edu.ifsp.souza.charles.pdm.mycontacts.model.Contact
 
 class MainActivity : AppCompatActivity() {
-
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     // Data source
     private val contactList: MutableList<Contact> = mutableListOf()
 
-    //Adaptar
-    private val contactAdpater = ContactAdapter
+    // Adapter
+    private lateinit var contactAdapter: ContactAdapter
 
-
-    private lateinit var  carl: ActivityResultLauncher<Intent>
+    private lateinit var carl: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
 
         fillContactList()
-        amb.contactsLv.adapter = contactAdpater
+        contactAdapter = ContactAdapter(this, contactList)
+        amb.contactsLv.adapter = contactAdapter
 
         carl = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult(),
         ) { result ->
-            if(result.resultCode == RESULT_OK){
-              val contact = result.data?.getParcelableExtra<Contact>(EXTRA_CONTACT)
-              contact?.let {  _contact ->
-                  contactList.add(_contact)
-                  contactAdpater.add(_contact.toString())
-                  contactAdpater.notifyDataSetChanged()
-              }
+            if (result.resultCode == RESULT_OK) {
+                val contact = result.data?.getParcelableExtra<Contact>(EXTRA_CONTACT)
+                contact?.let { _contact->
+                    contactList.add(_contact)
+                    contactAdapter.notifyDataSetChanged()
+                }
             }
         }
     }
-
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -55,20 +53,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId){
+        return when(item.itemId) {
             R.id.addContactMi -> {
                 carl.launch(Intent(this, ContactActivity::class.java))
-                // Abre a tela de contato
                 true
             }
-            else -> {false}
+            else -> { false }
         }
     }
 
-
-
-    private fun fillContactList(){
-        for ( i in 1 .. 10) {
+    private fun fillContactList() {
+        for (i in 1..10) {
             contactList.add(
                 Contact(
                     id = i,
