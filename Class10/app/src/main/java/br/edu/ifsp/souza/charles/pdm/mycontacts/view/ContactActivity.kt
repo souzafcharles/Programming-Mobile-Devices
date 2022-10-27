5a108c0c3b66621ac87ea613383ba091edcf5952
souzafcharles <souzach@outlook.com>
2022-10-26 21:32:54 -0300

    Example Class 10

---
+++ Class10/app/src/main/java/br/edu/ifsp/souza/charles/pdm/mycontacts/view/ContactActivity.kt
@@ -0,0 +1,46 @@
+package br.edu.ifsp.souza.charles.pdm.mycontacts.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.ifsp.souza.charles.pdm.mycontacts.databinding.ActivityContactBinding
+import br.edu.ifsp.souza.charles.pdm.mycontacts.model.Constant.EXTRA_CONTACT
+import br.edu.ifsp.souza.charles.pdm.mycontacts.model.Contact
+import kotlin.random.Random
+
+class ContactActivity : AppCompatActivity() {
+
+    private val acb: ActivityContactBinding by lazy {
+        ActivityContactBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(acb.root)
+
+        val receivedContact = intent.getParcelableExtra<Contact>(EXTRA_CONTACT)
+        receivedContact?.let{ _receivedContact ->
+            with(acb) {
+                with(_receivedContact){
+                    nameEt.setText(name)
+                    addressEt.setText(address)
+                    phoneEt.setText(phone)
+                    emailEt.setText(email)
+                }
+            }
+        }
+
+        acb.saveBt.setOnClickListener{
+            val contact = Contact (
+                id = Random(System.currentTimeMillis()).nextInt(),
+                name = acb.nameEt.text.toString(),
+                address = acb.addressEt.text.toString(),
+                phone = acb.phoneEt.text.toString(),
+                email = acb.emailEt.text.toString()
+            )
+            val resultIntent = Intent()
+            resultIntent.putExtra(EXTRA_CONTACT, contact)
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
+    }
+}
---
+++ Class10/app/src/main/java/br/edu/ifsp/souza/charles/pdm/mycontacts/view/MainActivity.kt
@@ -0,0 +1,107 @@
+package br.edu.ifsp.souza.charles.pdm.mycontacts.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView.AdapterContextMenuInfo
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.souza.charles.pdm.mycontacts.R
+import br.edu.ifsp.souza.charles.pdm.mycontacts.adapter.ContactAdapter
+import br.edu.ifsp.souza.charles.pdm.mycontacts.databinding.ActivityMainBinding
+import br.edu.ifsp.souza.charles.pdm.mycontacts.model.Constant.EXTRA_CONTACT
+import br.edu.ifsp.souza.charles.pdm.mycontacts.model.Contact
+
+class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    // Data source
+    private val contactList: MutableList<Contact> = mutableListOf()
+
+    // Adapter
+    private lateinit var contactAdapter: ContactAdapter
+
+    private lateinit var carl: ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(amb.root)
+
+        fillContactList()
+        contactAdapter = ContactAdapter(this, contactList)
+        amb.contactsLv.adapter = contactAdapter
+
+        carl = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+        ) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val contact = result.data?.getParcelableExtra<Contact>(EXTRA_CONTACT)
+                contact?.let { _contact->
+                    contactList.add(_contact)
+                    contactAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+        registerForContextMenu(amb.contactsLv)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.addContactMi -> {
+                carl.launch(Intent(this, ContactActivity::class.java))
+                true
+            }
+            else -> { false }
+        }
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        menuInflater.inflate(R.menu.context_menu_main, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val position = (item.menuInfo as AdapterContextMenuInfo).position
+        return when(item.itemId){
+            R.id.removeContactMi ->{
+                //Remove o contato
+                contactList.removeAt(position)
+                contactAdapter.notifyDataSetChanged()
+                true
+            }
+            R.id.editContactMi ->{
+                // Chama a tela para editar o contato
+                val contact = contactList[position]
+                val contactIntent = Intent(this, ContactActivity::class.java)
+                contactIntent.putExtra(EXTRA_CONTACT, contact)
+                startActivity(contactIntent)
+                true
+            }
+            else -> { false }
+        }
+    }
+
+    private fun fillContactList() {
+        for (i in 1..10) {
+            contactList.add(
+                Contact(
+                    id = i,
+                    name = "Nome $i",
+                    address = "Endereço $i",
+                    phone = "Telefone $i",
+                    email = "Email $i",
+                )
+            )
+        }
+    }
+}
