450ed6a3800116c306fe9a6ebcea7548658e5ffd
souzafcharles <souzach@outlook.com>
2022-11-23 20:42:59 -0300

    Class 13 Example

--- Class13/app/src/main/java/br/edu/ifsp/ads/pdm/mycontacts/model/database/ContactDAOFirebase.kt
+++ Class13/app/src/main/java/br/edu/ifsp/ads/pdm/mycontacts/model/database/ContactDAOFirebase.kt
@@ -2,13 +2,59 @@ package br.edu.ifsp.ads.pdm.mycontacts.model.database
 
 import br.edu.ifsp.ads.pdm.mycontacts.model.dao.ContactDAO
 import br.edu.ifsp.ads.pdm.mycontacts.model.entity.Contact
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 class ContactDAOFirebase: ContactDAO {
     private val CONTACT_DATABASE_ROOT_NODE = "contacts"
     private val contactRealtimeDatabase = Firebase.database.getReference(CONTACT_DATABASE_ROOT_NODE)
 
+    //Simula uma consulta ao banco de dados
+
+    private val contactList = mutableListOf<Contact>()
+    init {
+        contactRealtimeDatabase.addChildEventListener(object: ChildEventListener{
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                val contact: Contact? = snapshot.getValue<Contact>()
+
+                contact?.let { _contact ->
+                    if (!contactList.any { _contact.id == it.id })
+                        contactList.add(_contact)
+                }
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                val contact: Contact? = snapshot.getValue<Contact>()
+
+                contact?.let { _contact ->
+                    val position = contactList.indexOfFirst { _contact.id == it.id }
+                    if (position != -1) {
+                        contactList[position] = _contact
+                    }
+                }
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                val contact: Contact? = snapshot.getValue<Contact>()
+                contactList.remove(contact)
+            }
+
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                //This method does not apply in this application
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                //This method does not apply in this application
+            }
+
+
+        })
+    }
     override fun createContact(contact: Contact): Int {
         TODO("Not yet implemented")
     }
